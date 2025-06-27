ef4fc80573614eb40b56cb027f8f8674ee0e70bc
hikaru <827394320@qq.com>
2025-06-27 11:55:28 +0800

    ✨ refactor: refactor

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -6,7 +6,11 @@ it('retry', async () => {
     // 为每个任务创建独立的 mock 函数
     const taskMap = [1, 2, 3, 4, 5, 6].map((i) => {
         const mockTask = createMockAsyncTask()
-        return retry(() => mockTask(i), 5)
+        return retry(() => mockTask(i), {
+            taskName: '获取客流',
+            maxRetries: 5,
+            retryDelay: 500,
+        })
     })
 
     const results = await Promise.allSettled(taskMap)
